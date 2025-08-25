3df0a32406fe5d587ca5fc133f49d698bd4fad77
Atanu pal <atanupal@live.com>
2025-08-26 02:32:37 +0530

    add version to routes api url

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const billRoutes = require("./v1/billRoutes");
 
-router.use("/api", billRoutes);
+router.use("/api/v1", billRoutes);
 
 module.exports = router;
